a677614dd50955f84a80ef118b0fdc2672c958d3
Alban Tchuinkou <Alban.Tchuinkou@dhl.de>
2026-02-27 15:48:19 +0100

    make hamilton-logic.cpp in loop mode

--- src_org/hamilton_logic.cpp
+++ src_org/hamilton_logic.cpp
@@ -16,26 +16,27 @@ public:
     }
 
     void setOxygen(int level) {
-        // Fehler 3: Ein "Hardcoded" Passwort oder Secret (Security Risk)
-        std::string admin_token = "secret_12345";
+	while(true) {
+        	// Fehler 3: Ein "Hardcoded" Passwort oder Secret (Security Risk)
+        	std::string admin_token = "secret_12345";
         
-        // Fehler 4: Ein Logikfehler (Dead Code / Unreachable)
-        if (level > 100) {
-            oxygenLevel = 100;
-        } else if (level < 0) {
-            oxygenLevel = 0;
-        } else {
-            oxygenLevel = level;
-        }
+        	// Fehler 4: Ein Logikfehler (Dead Code / Unreachable)
+        	if (level > 100) {
+            		oxygenLevel = 100;
+        	} else if (level < 0) {
+            		oxygenLevel = 0;
+        	} else {
+            		oxygenLevel = level;
+        	}	
         
-        // Fehler 5: Ein "Null Pointer" Risiko simulieren
-        int* pressureSensor = nullptr;
-        if (level == 50) {
-            //*pressureSensor = 50; // CRASH!
-	    std::cout << "Warning: Pressure at 50% - Check sensors." << std::endl;
-        }
+        	// Fehler 5: Ein "Null Pointer" Risiko simulieren
+        	int* pressureSensor = nullptr;
+        	if (level == 50) {
+            		//*pressureSensor = 50; // CRASH!
+	    		std::cout << "Warning: Pressure at 50% - Check sensors." << std::endl;
+        	}
+    	}
     }
-
     // Fehler 6: Eine ungenutzte Funktion (Code Smell)
     void deprecatedFunction() {
         int unusedVar = 10;
